78e67ddc3f3aadb38345b63cb20ec579b4994e9b
Jonathan Winter <jonathan.winter@us.generaliglobalassistance.com>
2021-02-02 19:09:17 -0500

    completed section 4

---
+++ sections/4/PlayingCat/src/PlayingCat.java
@@ -0,0 +1,13 @@
+public class PlayingCat {
+
+    public static void main(String[] args) {
+        System.out.println(isCatPlaying(true, 10));
+        System.out.println(isCatPlaying(false, 36));
+        System.out.println(isCatPlaying(false, 35));
+    }
+
+    public static boolean isCatPlaying(boolean summer, int temperature) {
+        return (temperature >= 25 && temperature <= 35)
+                || (summer && (temperature >= 25 && temperature <= 45));
+    }
+}
