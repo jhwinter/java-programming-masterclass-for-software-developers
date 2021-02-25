42076de44c1b7a6262fe4f262e180b362523f45c
Jonathan Winter <jonathan.winter@us.generaliglobalassistance.com>
2021-02-25 08:55:24 -0500

    completed Wall challenge

---
+++ sections/6/Wall/src/academy/learnprogramming/Wall.java
@@ -0,0 +1,35 @@
+package academy.learnprogramming;
+
+public class Wall {
+    private double width;
+    private double height;
+
+    public Wall() {
+        this(2, 3);
+    }
+
+    public Wall(double width, double height) {
+        this.width = width < 0 ? 0 : width;
+        this.height = height < 0 ? 0 : height;
+    }
+
+    public double getWidth() {
+        return this.width;
+    }
+
+    public void setWidth(double width) {
+        this.width = width < 0 ? 0 : width;
+    }
+
+    public double getHeight() {
+        return this.height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height < 0 ? 0 : height;
+    }
+
+    public double getArea() {
+        return getWidth() * getHeight();
+    }
+}
