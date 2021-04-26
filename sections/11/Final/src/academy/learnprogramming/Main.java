234f65c6f7189dc29f7eeca4aaec3969ac3b259a
Jonathan Winter <jonathan.winter@us.generaliglobalassistance.com>
2021-04-26 14:37:25 -0400

    final

--- sections/11/Final/src/academy/learnprogramming/Main.java
+++ sections/11/Final/src/academy/learnprogramming/Main.java
@@ -15,15 +15,20 @@ public class Main {
         System.out.println(Math.PI);
         // Math m = new Math();
 
-        int pw = 674312;
-        Password password = new Password(pw);
-        password.storePassword();
+        // int pw = 674312;
+        // Password password = new ExtendedPassword(pw);
+        // password.storePassword();
+        //
+        // password.letMeIn(1);
+        // password.letMeIn(523266);
+        // password.letMeIn(9773);
+        // password.letMeIn(0);
+        // password.letMeIn(-1);
+        // password.letMeIn(674312);
 
-        password.letMeIn(1);
-        password.letMeIn(523266);
-        password.letMeIn(9773);
-        password.letMeIn(0);
-        password.letMeIn(-1);
-        password.letMeIn(674312);
+        System.out.println("Main method called");
+        SIBTest test = new SIBTest();
+        test.someMethod();
+        System.out.println("Owner is " + SIBTest.owner);
     }
 }
