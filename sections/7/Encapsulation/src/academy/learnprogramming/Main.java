943d1f0e038520fef65c468b843fd8ff88c72e99
jhwinter <14960780+jhwinter@users.noreply.github.com>
2023-03-20 17:23:24 -0400

    Encapsulation

--- sections/7/Encapsulation/src/academy/learnprogramming/Main.java
+++ sections/7/Encapsulation/src/academy/learnprogramming/Main.java
@@ -17,8 +17,8 @@ public class Main {
         // player.loseHealth(damage);
         // System.out.println("Remaining health = " + player.healthRemaining());
         //
-        // EnhancedPlayer player = new EnhancedPlayer("Tim", 50, "Sword");
-        // System.out.println("Initial health is " + player.getHealth());
+        EnhancedPlayer player = new EnhancedPlayer("Tim", 50, "Sword");
+        System.out.println("Initial health is " + player.getHealth());
 
 
         // Challenge Exercise
