93e9358828132b81764787e53e8d7512d333edee
jhwinter <14960780+jhwinter@users.noreply.github.com>
2023-03-20 17:23:23 -0400

    completed Complex Operations challenge

---
+++ sections/6/ComplexOperations/src/academy/learnprogramming/ComplexNumber.java
@@ -0,0 +1,37 @@
+package academy.learnprogramming;
+
+public class ComplexNumber {
+    private double real;
+    private double imaginary;
+
+    public ComplexNumber(double real, double imaginary) {
+        this.real = real;
+        this.imaginary = imaginary;
+    }
+
+    public double getReal() {
+        return this.real;
+    }
+
+    public double getImaginary() {
+        return this.imaginary;
+    }
+
+    public void add(double real, double imaginary) {
+        this.real = this.getReal() + real;
+        this.imaginary = this.getImaginary() + imaginary;
+    }
+
+    public void add(ComplexNumber other) {
+        this.add(other.getReal(), other.getImaginary());
+    }
+
+    public void subtract(double real, double imaginary) {
+        this.real = this.getReal() - real;
+        this.imaginary = this.getImaginary() - imaginary;
+    }
+
+    public void subtract(ComplexNumber other) {
+        this.subtract(other.getReal(), other.getImaginary());
+    }
+}
