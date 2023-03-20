3b09b4efd4585e1831b377c5076ab633052e6ed3
jhwinter <14960780+jhwinter@users.noreply.github.com>
2023-03-20 17:23:21 -0400

    completed section 4

---
+++ sections/4/MinutesToYearsDaysCalculator/src/MinutesToYearsDaysCalculator.java
@@ -0,0 +1,20 @@
+public class MinutesToYearsDaysCalculator {
+    private static final String INVALID_VALUE = "Invalid Value";
+
+    public static void main(String[] args) {
+        printYearsAndDays(525600);
+        printYearsAndDays(1051200);
+        printYearsAndDays(561600);
+    }
+
+    public static void printYearsAndDays(long minutes) {
+        if (minutes < 0) {
+            System.out.println(INVALID_VALUE);
+        } else {
+            long days = ((minutes / 60) / 24);
+            long years = days / 365;
+            long remainingDays = days % 365;
+            System.out.println(minutes + " min = " + years + " y and " + remainingDays + " d");
+        }
+    }
+}
