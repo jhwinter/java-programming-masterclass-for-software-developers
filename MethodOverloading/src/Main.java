4a6de3717d373d0acf69f515bb4f5a5e4810b795
jhwinter <14960780+jhwinter@users.noreply.github.com>
2023-03-20 17:23:21 -0400

    working on section 4

---
+++ MethodOverloading/src/Main.java
@@ -0,0 +1,47 @@
+public class Main {
+    public static void main(String[] args) {
+        int newScore = calculateScore("Tim", 500);
+        System.out.println("New score is " + newScore);
+
+        calculateScore(75);
+        calculateScore();
+
+        calcFeetAndInchesToCentimeters(6, 7);
+        calcFeetAndInchesToCentimeters(156);
+    }
+
+    public static int calculateScore(String playerName, int score) {
+        System.out.println("Player " + playerName + " scored " + score + " points");
+        return score * 1000;
+    }
+
+    public static int calculateScore(int score) {
+        System.out.println("Unnamed Player scored " + score + " points");
+        return score * 1000;
+    }
+
+    public static int calculateScore() {
+        System.out.println("No player name, no player score");
+        return 0;
+    }
+
+    public static double calcFeetAndInchesToCentimeters(double feet, double inches) {
+        if (feet < 0 || (inches < 0 || inches > 12)) {
+            System.out.println("Invalid feet or inches parameters");
+            return -1;
+        }
+        double centimeters = ((feet * 12) + inches) * 2.54;
+        System.out.println(feet + " ft, " + inches + " in = " + centimeters + " cm");
+        return centimeters;
+    }
+
+    public static double calcFeetAndInchesToCentimeters(double inches) {
+        if (inches < 0) {
+            System.out.println("Invalid inches parameter");
+            return -1;
+        }
+        double feet = (int) (inches / 12);
+        double remainingInches = (int) inches % 12;
+        return calcFeetAndInchesToCentimeters(feet, remainingInches);
+    }
+}
