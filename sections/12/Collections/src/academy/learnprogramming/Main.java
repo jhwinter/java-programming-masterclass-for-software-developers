d959f3aa38c6c50d3b3d7c0f58106f241ea342c8
jhwinter <14960780+jhwinter@users.noreply.github.com>
2023-03-20 17:23:28 -0400

    156. Comparable and Comparator

--- sections/12/Collections/src/academy/learnprogramming/Main.java
+++ sections/12/Collections/src/academy/learnprogramming/Main.java
@@ -8,34 +8,54 @@ public class Main {
 
     public static void main(String[] args) {
         Theatre theatre = new Theatre("Olympian", 8, 12);
-        List<Theatre.Seat> seatCopy = new ArrayList<>(theatre.seats);  // shallow copy, only copies the reference to the elements
-        printList(seatCopy);
+        // List<Theatre.Seat> seatCopy = new ArrayList<>(theatre.seats);  // shallow copy, only copies the reference to the elements
+        // printList(seatCopy);
 
-        seatCopy.get(1).reserve();
-        if (theatre.reserveSeat("A02")) {
-            System.out.println("Please pay for A02");
+        // seatCopy.get(1).reserve();
+        if (theatre.reserveSeat("D12")) {
+            System.out.println("Please pay for D12");
+        } else {
+            System.out.println("Seat already reserved");
+        }
+        if (theatre.reserveSeat("D12")) {
+            System.out.println("Please pay for D12");
+        } else {
+            System.out.println("Seat already reserved");
+        }
+        if (theatre.reserveSeat("B13")) {
+            System.out.println("Please pay for B13");
         } else {
             System.out.println("Seat already reserved");
         }
 
-        Collections.shuffle(seatCopy);
-        System.out.println("Printing seatCopy");
-        printList(seatCopy);
-        System.out.println("Printing theatre.seats");
-        printList(theatre.seats);
+        List<Theatre.Seat> reverseSeats = new ArrayList<>(theatre.getSeats());
+        Collections.reverse(reverseSeats);
+        printList(reverseSeats);
 
-        Theatre.Seat minSeat = Collections.min(seatCopy);
-        Theatre.Seat maxSeat = Collections.max(seatCopy);
-        System.out.println("Min seat number is " + minSeat.getSeatNumber());
-        System.out.println("Max seat number is " + maxSeat.getSeatNumber());
+        List<Theatre.Seat> priceSeats = new ArrayList<>(theatre.getSeats());
+        priceSeats.add(theatre.new Seat("B00", 13.00));
+        priceSeats.add(theatre.new Seat("A00", 13.00));
+        Collections.sort(priceSeats, Theatre.PRICE_ORDER);
+        printList(priceSeats);
 
-        sortList(seatCopy);
-        System.out.println("Printing sorted seatCopy");
-        printList(seatCopy);
-
-        // this does not work to deep copy objects
-        List<Theatre.Seat> newList = new ArrayList<>(theatre.seats.size());
-        Collections.copy(newList, theatre.seats);
+        // Collections.shuffle(seatCopy);
+        // System.out.println("Printing seatCopy");
+        // printList(seatCopy);
+        // System.out.println("Printing theatre.seats");
+        // printList(theatre.seats);
+        //
+        // Theatre.Seat minSeat = Collections.min(seatCopy);
+        // Theatre.Seat maxSeat = Collections.max(seatCopy);
+        // System.out.println("Min seat number is " + minSeat.getSeatNumber());
+        // System.out.println("Max seat number is " + maxSeat.getSeatNumber());
+        //
+        // sortList(seatCopy);
+        // System.out.println("Printing sorted seatCopy");
+        // printList(seatCopy);
+        //
+        // // this does not work to deep copy objects
+        // List<Theatre.Seat> newList = new ArrayList<>(theatre.seats.size());
+        // Collections.copy(newList, theatre.seats);
 
         // // theatre.getSeats();
         // if (theatre.reserveSeat("B13")) {
@@ -52,19 +72,19 @@ public class Main {
 
     public static void printList(List<Theatre.Seat> list) {
         for (Theatre.Seat seat : list) {
-            System.out.print(" " + seat.getSeatNumber());
+            System.out.print(" " + seat.getSeatNumber() + " $" + seat.getPrice());
         }
         System.out.println();
         System.out.println("==============================================");
     }
 
-    public static void sortList(List<? extends Theatre.Seat> list) {
-        for (int i = 0; i < list.size() - 1; i++) {
-            for (int j = i + 1; j < list.size(); j++) {
-                if (list.get(i).compareTo(list.get(j)) > 0) {
-                    Collections.swap(list, i, j);
-                }
-            }
-        }
-    }
+    // public static void sortList(List<? extends Theatre.Seat> list) {
+    //     for (int i = 0; i < list.size() - 1; i++) {
+    //         for (int j = i + 1; j < list.size(); j++) {
+    //             if (list.get(i).compareTo(list.get(j)) > 0) {
+    //                 Collections.swap(list, i, j);
+    //             }
+    //         }
+    //     }
+    // }
 }
