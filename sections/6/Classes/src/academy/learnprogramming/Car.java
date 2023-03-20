2a0384abd8f44972d86a1f7fcc419f9cdf844d05
jhwinter <14960780+jhwinter@users.noreply.github.com>
2023-03-20 17:23:22 -0400

    Clases part 1

---
+++ sections/6/Classes/src/academy/learnprogramming/Car.java
@@ -0,0 +1,28 @@
+package academy.learnprogramming;
+
+/**
+ *
+ */
+public class Car {
+    private int doors;
+    private int wheels;
+    private String model;
+    private String engine;
+    private String color;
+
+    // public void setModel(String model) {
+    //     this.model = model;
+    // }
+    public void setModel(String model) {
+        String validModel = model.toLowerCase();
+        if (validModel.equals("carrera") || validModel.equals("commodore")) {
+            this.model = model;
+        } else {
+            this.model = "Unknown";
+        }
+    }
+
+    public String getModel() {
+        return this.model;
+    }
+}
