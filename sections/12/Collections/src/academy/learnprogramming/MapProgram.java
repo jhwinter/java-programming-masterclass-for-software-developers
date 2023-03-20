ad188f2d59e2eb34c21a009c476485f59b3fa9d9
jhwinter <14960780+jhwinter@users.noreply.github.com>
2023-03-20 17:23:29 -0400

    Map Continued

--- sections/12/Collections/src/academy/learnprogramming/MapProgram.java
+++ sections/12/Collections/src/academy/learnprogramming/MapProgram.java
@@ -29,6 +29,22 @@ public class MapProgram {
 
         System.out.println("=====================================");
 
+        // languages.remove("Lisp");
+        if (languages.remove("Algol", "an algorithmic language")) {
+            System.out.println("Algol removed");
+        } else {
+            System.out.println("Algol not removed, key/value pair not found");
+        }
+
+        // System.out.println(languages.replace("Lisp", "a functional programming language with imperative features"));
+        if (languages.replace("Lisp", "Therein lies madness", "a functional programming language with imperative features")) {
+            System.out.println("Lisp replaced");
+        } else {
+            System.out.println("Lisp was not replaced");
+        }
+        // System.out.println(languages.replace("Scala", "this will not be added"));
+
+
         for (String key : languages.keySet()) {
             System.out.println(key + " : " + languages.get(key));
         }
