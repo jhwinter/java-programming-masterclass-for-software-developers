5c6dc598abb6e5bf64b9b2f1a9a85dc4f00c03eb
jhwinter <14960780+jhwinter@users.noreply.github.com>
2023-03-20 17:23:22 -0400

    section 5

---
+++ sections/5/FlourPacker/src/FlourPacker.java
@@ -0,0 +1,19 @@
+public class FlourPacker {
+    public static void main(String[] args) {
+        System.out.println("canPack (1, 0, 4) - " + canPack(1, 0, 4));
+        System.out.println("canPack (1, 0, 5) - " + canPack(1, 0, 5));
+        System.out.println("canPack (0, 5, 4) - " + canPack(0, 5, 4));
+        System.out.println("canPack (2, 2, 11) - " + canPack(2, 2, 11));
+        System.out.println("canPack (-3, 2, 12) - " + canPack(-3, 2, 12));
+    }
+
+    public static boolean canPack(int bigCount, int smallCount, int goal) {
+        if (bigCount < 0 || smallCount < 0 || goal < 0) {
+            return false;
+        }
+        if (bigCount * 5 + smallCount < goal) {
+            return false;
+        }
+        return smallCount >= goal % 5;
+    }
+}
